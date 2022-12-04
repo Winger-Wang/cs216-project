875bbee38ff447735d716ed23bce2d344dba181f
IanMei <70625172+IanMei@users.noreply.github.com>
2022-12-03 22:50:24 -0500

    Update dic2csv.py

--- prototype/dic2csv.py
+++ prototype/dic2csv.py
@@ -337,3 +337,80 @@ def read_Related(l, relateds, i = 0, filename = "Related.csv"):
                     writer.writerow(related.values())
         
         l.append(i)
+        
+def read_Reply(l,Reply,i=0,filename=""):
+    read_Reply_main(l,Reply['page'],i,filename)
+    read_Reply_replies(l,Reply['replies'],i,filename)
+
+def read_Reply_main(l, Reply, i=0, filename="Reply_main.csv"):
+    lock = threading.Lock()
+    
+    with lock:
+        
+        # print('reading tags')
+
+        filename= filename + "Reply_main.csv"
+        
+        if os.path.exists(filename):
+            
+            with open(filename, 'a', encoding = "utf-8-sig", newline="") as myFile:
+
+                writer = csv.writer(myFile)
+
+                Reply["aid"]=i
+                
+                writer.writerow(Reply.values())
+            
+        else:
+        
+            with open(filename, 'w', encoding = "utf-8-sig", newline="") as myFile:
+
+                writer = csv.writer(myFile)
+                
+                ## Your main work is to change this part
+                
+                writer.writerow(["account", "count", "num", "size"])
+
+                Reply["aid"]=i
+
+                writer.writerow(Reply.values())
+        l.append(i)
+                
+def read_Reply_replies(l, Reply, i=0, filename="Reply_replies.csv"):
+    lock = threading.Lock()
+    
+    with lock:
+        
+        # print('reading tags')
+
+        filename= filename + "Reply_replies.csv"
+        
+        if os.path.exists(filename):
+            
+            with open(filename, 'a', encoding = "utf-8-sig", newline="") as myFile:
+
+                writer = csv.writer(myFile)
+
+
+                
+                for items in Reply:
+
+                    items["aid"]=i
+                    writer.writerow(items.values())
+            
+        else:
+        
+            with open(filename, 'w', encoding = "utf-8-sig", newline="") as myFile:
+
+                writer = csv.writer(myFile)
+                
+                ## Your main work is to change this part
+                
+                for items in Reply:
+
+                    items["aid"]=i
+                
+                writer.writerow(Reply[0].keys())
+                
+                for items in Reply:
+                    writer.writerow(items.values())
