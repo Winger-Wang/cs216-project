cdcc343e9f9b8505d71a2e0ee548b8f2468fa645
winger216 <winger_wang@outlook.com>
2022-12-06 23:05:23 +0000

    minor fix in Reply

--- prototype/dic2csv.py
+++ prototype/dic2csv.py
@@ -341,6 +341,10 @@ def read_Related(l, relateds, i = 0, filename = "Related.csv"):
 
 #### read_Reply by Eric#####
 def read_Reply(l,Reply,i=0,filename=""):
+    if Reply == None:
+        return
+    else:
+        Reply[0]['aid'] = i
     read_Reply_main(l,Reply['page'],i,filename)
     read_Reply_replies(l,Reply['replies'],i,filename)
 
@@ -391,10 +395,7 @@ def read_Reply_main(l, Reply, i=0, filename="Reply_main.csv"):
 def read_Reply_replies(l, Reply, i=0, filename="Reply_replies.csv"):
     lock = threading.Lock()
     
-    if Reply == None:
-        return
-    else:
-        Reply[0]['aid'] = i
+    
     
     header = Reply[0].keys()
     
